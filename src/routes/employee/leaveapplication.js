021a360f08207d1822e2da5f4da48c25861705a5
shruthi2306 <shruthi.srinivasan01@sjsu.edu>
2021-12-08 19:43:04 -0800

    API changes made

--- src/routes/employee/leaveapplication.js
+++ src/routes/employee/leaveapplication.js
@@ -6,7 +6,7 @@ var  mongoose = require("mongoose");
 var jwt = require("jsonwebtoken");
 require('dotenv').config();
 const router=express.Router();
-import verifyEmployee from '../../utils/employee/verifyEmployee'
+//import verifyEmployee from '../../utils/employee/verifyEmployee'
 router.get("/",function(req,res){
     res.send("Leave Experience details")
 })
@@ -141,4 +141,39 @@ router.get("/leave-application-emp/:id", verifyEmployee, (req, res) => {
       });
     }
   );
+  function verifyEmployee(req, res, next) {
+    console.log(req.headers["authorization"]);
+    const Header = req.headers["authorization"];
+    console.log("verifyEm-->p",Header)
+  
+    if (typeof Header !== "undefined") {
+      // decodedData = jwt.decode(req.headers['authorization']);
+      // if(decodedData.Account)
+      jwt.verify(Header, jwtKey, (err, authData) => {
+        if (err) {
+          console.log("403-->",err)
+          res.sendStatus(403);
+        } else {
+          console.log("CHECK-->",authData._id == req.params.id)
+          if (authData._id == req.params.id) {
+            console.log(authData);
+            if (authData.Account == 3) {
+              next();
+            } else {
+              console.log("auth--->",)
+              res.sendStatus(403);
+            }
+          } else {
+            console.log("forbidden--->",authData,req.params.id)
+  
+            res.sendStatus(403);
+          }
+        }
+      });
+    } else {
+      // Forbidden
+      console.log("forbidden")
+      res.sendStatus(403);
+    }
+  }
 module.exports=router  
